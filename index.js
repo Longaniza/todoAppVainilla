52f32415d4b56ab1180549496b730f749fc97fe9
Ivanov <ramireztiradojose@gmail.com>
2020-09-09 13:21:53 -0700

    Delete function added

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const uiManagement = (() => {
+
+    const addEvents = (todos) => {
+        todos.forEach(todo => {
+            document.getElementById(todo.id).querySelector(".delete-button").addEventListener("click", () => {
+                const elemento = document.getElementById(todo.id);
+                elemento.parentNode.removeChild(elemento);
+                data.deleteTodo(todo);
+            })
+        })
+    }
     const add = (todo) => {
-        document.getElementById("lista").innerHTML += '<input type="text" class="label-look" value="' + todo.value +'"> <button type="button">Edit</button> <button type="button">Borrar</button> <br>'
+        document.getElementById("lista").innerHTML += '<div id="' + todo.id+ '"><input type="text" class="label-look" value="' + todo.value +
+        '"> <button type="button">Edit</button> <button class="delete-button" type="button">Borrar</button> <br></div>'
     }
     return {
-        add
+        add,
+        addEvents,
     }
 })();
 
@@ -13,18 +25,30 @@ const data = (() => {
    (() => todos.forEach(todo => {
        uiManagement.add(todo);
    }))();
+
+   (() => uiManagement.addEvents(todos))();
    
    const getTodos = () => {
     return todos;
     }
+
    const add = (todo) => {
        uiManagement.add(todo);
        todos.push(todo);
        localStorage.setItem('todos',JSON.stringify(getTodos()));
+       uiManagement.addEvents(todos);
    }
+   const deleteTodo = (todo) => {
+        const todoIdx = todos.map(element => element.id).indexOf(todo.id);
+        todos.splice(todoIdx, 1);
+        localStorage.setItem('todos',JSON.stringify(getTodos()));
+    }
+
+   
    return {
        add,
-       getTodos
+       getTodos,
+       deleteTodo
    } 
 })();
 
